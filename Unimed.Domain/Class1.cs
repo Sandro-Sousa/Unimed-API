1ba04ad4e2377e041fadb317a0a3e180a80dd311
Sandro-Sousa <sandro-souza-oliveira@hotmail.com>
2021-02-18 15:53:54 -0300

    Iniciando ProjetoTeste

---
+++ Unimed.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Unimed.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Unimed.Interface/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Unimed.Interface
+{
+    public class Class1
+    {
+    }
+}
