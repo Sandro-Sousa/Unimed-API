2ef30d92559782b953f75e220ee42f3cd1f4f56c
Sandro-Sousa <sandro-souza-oliveira@hotmail.com>
2021-02-19 21:19:29 -0300

    Adicionado conexão de criação do banco de dados

---
+++ Unimed.Repository/Commom/UnimedDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Unimed.Repository
+{
+    public class UnimedDbContextFactory : IDesignTimeDbContextFactory<UnimedDbContext>
+    {
+        public UnimedDbContext CreateDbContext(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var fileName = Directory.GetCurrentDirectory() + $"/../Unimed.API/appsettings.{environmentName}.json";
+
+            var configuration = new ConfigurationBuilder().AddJsonFile(fileName).Build();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var builder = new DbContextOptionsBuilder<UnimedDbContext>();
+            builder.UseSqlServer(connectionString);
+
+            return new UnimedDbContext(builder.Options);
+        }
+    }
+}
